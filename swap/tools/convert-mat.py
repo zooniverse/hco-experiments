4f5a68b8d95eb0551b5308678ebcb1e8fd197180
Michael Laraia <micheal.laraia@gmail.com>
2017-02-03 18:47:10 -0600

    convert matfile to csv

--- swap/tools/convert-mat.py
+++ swap/tools/convert-mat.py
@@ -3,15 +3,54 @@
 # Script to convert .mat file to a csv
 
 import scipy.io as sio
+import os
+import numpy
+import csv
+import sys
 
 def main():
     path = os.path.dirname(os.path.realpath(__file__))
     fname = 'SNHunters_classification_dump_20170109_metadata.mat'
-    out = 'SNHunters_classification_dump_20170109_metadata.csv'
+    outfile = 'SNHunters_classification_dump_20170109_metadata.csv'
 
     data = sio.loadmat('/'.join((path,fname)))
-    out = '/'.join((path,out))
+    outfile = '/'.join((path,outfile))
 
-    keys = ['classification_id', 'user_name','user_id','annotation','gold_label','machine_score','diff','object_id','subject_id','mag','mag_err']
+    keys = ['classification_id', 'user_name','user_id',\
+            'annotation','gold_label','machine_score', \
+            'diff','object_id','subject_id','mag','mag_err']
 
-    for i in range(len(data['classification_id'][0])):
+    count = 0
+
+    with open(outfile, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=keys)
+        writer.writeheader()
+            
+
+        for index in range(len(data['classification_id'][0])):
+            d = {}
+
+            for key in keys:
+                #print(key, data[key], type(data[key][0]))
+                if type(data[key][0]) is numpy.str_:
+                    d[key] = data[key][index].strip()
+                else:
+                    d[key] = data[key][0][index]
+            
+
+            writer.writerow(d)
+
+            sys.stdout.write("%d records processed\r" % count)
+            sys.stdout.flush()
+
+            count += 1
+
+
+
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
