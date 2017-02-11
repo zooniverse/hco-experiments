81361dd37e7159f798764fc9dd7e57c61a479064
Marco Willi <will5448@umn.edu>
2017-02-10 18:28:01 -0600

    new swap, first working version

--- swap/swap/swap.py
+++ swap/swap/swap.py
@@ -2,28 +2,6 @@
 # SWAP implementation
 # 
 
-#import modules
-import _pickle as pickle
-import scipy.io as sio
-import swap.Server
-
-
-# read classifications
-def test_swap():
-    n_classifications=1000
-    max_batch_size = 1000
-    server = swap.Server(.5,.5)
-    # initialize curser with limit and max batch size
-    classification_cursor = server.classifications.find().limit(n_classifications).batch_size(min(max_batch_size,n_classifications))
-    swappy = SWAP()
-    # loop over cursor to retrieve classifications
-    for i in range(0,8):
-        current_classification = classification_cursor.next()
-        swappy.processOneClassification(current_classification)
-    swappy.getUserData()
-    
-    
-
 class SWAP(object):
     """
         SWAP implementation, which calculates and updates a confusion matrix for each user
@@ -127,94 +105,60 @@ class SWAP(object):
     def updateSubjectData(self,cl):
         # check if subject is new and create in user dictionary if yes
         if cl['subject_id'] not in self.subjects:
-            self.subjects[cl['subject_id']] = {
-                      'annotations':[],
-                      'gold_labels':[],
-                      'labels':dict(),
-                      'probability_history':dict(),
-                      'n_classified':0,
-                      'probability_current':dict()}
+            current_subject = {'gold_label': cl['gold_label'],
+                      'annotation_history':[],
+                      'user_probabilities':[],
+                      'probability_labels':{'1':self.p0,'0':1-self.p0},
+                      'current_label':'',
+                      'current_max_prob':self.p0,
+                      'max_prob_history':[self.p0]}
             
-        # update label data
+            self.subjects[cl['subject_id']] = current_subject
+            
+            
+        # get current user data
         current_user = self.users[cl['user_name']]
-        current_user['annotations'].append(cl['annotation'])
-        current_user['gold_labels'].append(cl['gold_label'])
-        current_user['n_classified'] += 1
         
-        # check if gold label exists and create if not
-        if not cl['gold_label'] in current_user['probability_current']:
-            current_user['probability_history'][cl['gold_label']] = []
-            current_user['probability_current'][cl['gold_label']] = self.epsilon
-            current_user['labels'][cl['gold_label']] = {'n':0,'n_match':0}
+        # update current annotation
+        current_subject= self.subjects[cl['subject_id']]
+        current_subject['annotation_history'].append(cl['annotation'])
+        # TODO: if user does not exist there us no current_user (if user has no gold label classification)
+        current_subject['user_probabilities'].append(current_user['probability_current'][cl['annotation']])
         
-        # check if annotation label exists and create if not
-        if not cl['annotation'] in current_user['probability_current']:
-            current_user['probability_history'][cl['annotation']] = []
-            current_user['probability_current'][cl['annotation']] = self.epsilon
-            current_user['labels'][cl['annotation']] = {'n':0,'n_match':0}
-            
-        # update number of subjects of that label seen by user
-        current_user['labels'][cl['gold_label']]['n'] += 1
+        # add current probability to class label
+        #user_prob = current_user['probability_current'][cl['annotation']]
         
-        # update number of matches of that label seen by user
-        if cl['gold_label'] == cl['annotation']:
-            current_user['labels'][cl['gold_label']]['n_match'] += 1
-           
-        # update user probability  
-        n_classified_of_that_label = current_user['labels'][cl['gold_label']]['n']
-        n_correctly_classified = current_user['labels'][cl['gold_label']]['n_match']
-        p_classified_correctly = n_correctly_classified / n_classified_of_that_label
-        
-        # save updated probability
-        current_user['probability_history'][cl['gold_label']].append(p_classified_correctly)
-        current_user['probability_current'][cl['gold_label']] = p_classified_correctly 
+        # update success probability
+        sub_pos = current_subject['current_max_prob']
+        user_pos = current_user['probability_current']['1']
+        sub_times_user = float(sub_pos) * float(user_pos)
+        user_fail = current_user['probability_current']['0']
                     
-           
+        # TODO: change hard-coded annotation labels
+        # if positive annotation
+        if cl['annotation'] == '1':                                 
+            sub_pos_new = sub_times_user / (sub_times_user + (1-user_fail) * (1-sub_pos))
+        # if 
+        elif cl['annotation'] == '0':
+            sub_pos_new = (sub_pos * (1-user_pos)) / (sub_pos * (1-user_pos) + (user_fail * (1-sub_pos)))
+
+        current_subject['probability_labels']['1']=sub_pos_new
+        current_subject['current_max_prob'] = sub_pos_new
+        current_subject['current_label'] = '1' if sub_pos_new > 0.5 else '0'
+        current_subject['max_prob_history'].append(sub_pos_new)
+
+    def getSubjectData(self):
+        return self.subjects                          
         
     # Process a classification
     def processOneClassification(self,cl):
-        # if subject is gold standard update user success probability
-        if cl['gold_label'] in ('0','1'):
-                self.updateUserData(cl)               
-        # update subject probability              
-#        if cl['annotation'] == '1':
-#            # update probability that subject is of interest (=1), given the users accuracy
-#            self.S[subject_index] = self.S[subject_index] * self.M[user_index][1] / \
-#                (self.S[subject_index] * self.M[user_index][1] +
-#                 (1 - self.M[user_index][0]) * (1 - self.S[subject_index]))
-#        elif cl['annotation']== '0':
-#            # update probability that subject is not of interest (=0), given the users accuracy
-#            self.S[subject_index] = self.S[subject_index] * (1 - self.M[user_index][1]) / \
-#                (self.S[subject_index] * (1 - self.M[user_index][1]) +
-#                 (self.M[user_index][0]) * (1 - self.S[subject_index]))
+        # if subject is gold standard and gold_updates are specified, 
+        # update user success probability
+        if ((cl['gold_label'] in ('0','1')) and self.gold_updates):
+                self.updateUserData(cl) 
+        # update Subject probability
+        self.updateSubjectData(cl)
     
-
-    def save(self, filename):
-        """
-            Save the processed data to pickled mat files. Creates two
-            files, one for the user_history and one for the subject_history
-
-            Args:
-                filename (str):
-                    filename suffix to store the data in. replaces the extension with .pkl
-                    ex. 'foo.mat' will create 'user_dict_foo.pkl' and 'subject_dict_foo.pkl'
-        """
-        sio.savemat(filename, {"subjects": self.subjects,
-                               "p0": self.p0,
-                               "epsilon": self.epsilon,
-                               "unique_users": self.unique_users,
-                               "M": self.M,
-                               "S": self.S,
-                               "dt": self.dt,
-                               "dt_prime": self.dt_prime})
-        # FIXME: this won't work with paths, only with filenames
-        out = open("user_dict_" + filename[:-4] + ".pkl", "wb")
-        pickle.dump(self.user_history, out)
-        out.close()
-
-        out = open("subject_dict_" + filename[:-4] + ".pkl", "wb")
-        pickle.dump(self.subject_history, out)
-        out.close()
 
 
 
---
+++ swap/test/swap_test.py
@@ -0,0 +1,20 @@
+from swap import SWAP
+            
+# read classifications
+def test_swap():
+    n_classifications=1000
+    max_batch_size = 1000
+    server = Server(.5,.5)
+    # initialize curser with limit and max batch size
+    classification_cursor = server.classifications.find().limit(n_classifications).batch_size(min(max_batch_size,n_classifications))
+    swappy = SWAP()
+    # loop over cursor to retrieve classifications
+    for i in range(0,8):
+        current_classification = classification_cursor.next()
+        swappy.processOneClassification(current_classification)
+    swappy.getUserData()
+    swappy.getSubjectData()
+    
+    
+if __name__ == "__main__":
+    test_swap()
