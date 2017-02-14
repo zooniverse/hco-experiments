4f1b643bfc4628fa8d71fcc16503ad4da40ac608
Michael Laraia <micheal.laraia@gmail.com>
2017-02-13 23:43:33 -0600

    starting work on agent and tracker classes

---
+++ swap/swap/agent.py
@@ -0,0 +1,70 @@
+################################################################
+# User agent, keeps track of a user's history and
+# score
+
+
+class Agent:
+
+    def __init__(self, epsilon, user_name):
+        self.user_name = user_name
+        self.epsilon = epsilon
+
+        self.annotations = []
+        self.gold_labels = []
+        self.labels = {}
+
+        self.prob_true = Prob_Tracker(1, self.epsilon)
+        self.prob_false = Prob_Tracker(0, self.epsilon)
+
+        self.n_classified = 0
+
+    def setEpsilon(self, epsilon):
+        pass
+
+    def addClassification(self, cl):
+        # Increment basic tracking
+        self.annotations.append(cl['annotation'])
+        self.gold_labels.append(cl['gold_label'])
+        self.n_classified += 1
+
+        # Decide which tracker to user
+        if cl['gold_label'] == 1:
+            prob = prob_true
+        elif cl['gold_label'] == 0:
+            prob = prob_false
+
+        # Add classification to tracker
+        prob.addClassification(cl['annotation'])
+        # Calculate and store new user probability
+        prob.calculateScore()
+
+    def getHistory(self):
+        pass
+
+    def updateProbability(self):
+        pass
+
+
+class Prob_Tracker:
+
+    def __init__(self, label, epsilon):
+        self.label = label
+
+        self.p_history = []
+        self.p_current = epsilon
+
+        self.n_seen = 0
+        self.n_matched = 0
+
+    def calculateScore(self):
+        p = self.n_seen / self.n_matched
+
+        self.p_history.append(p)
+        self.p_current = p
+
+        return p
+
+    def addClassification(self, matched):
+        self.n_seen += 1
+        if matched:
+            self.n_matched += 1
