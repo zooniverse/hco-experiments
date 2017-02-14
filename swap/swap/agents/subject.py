46a01eebf3a79cc23b549899f4be0acaca91731c
Michael Laraia <micheal.laraia@gmail.com>
2017-02-14 16:56:51 -0600

    working on porting swap to agents

--- swap/swap/agents/subject.py
+++ swap/swap/agents/subject.py
@@ -1,3 +1,85 @@
 ################################################################
 # Subject agent, keeps track of a subject's history and
 # score
+
+class Subject(Agent):
+
+    def __init__(self, subject_id, p0):
+        self.name = subject_id
+        self.p0 = p0
+
+        self.annotations = []
+        self.user_probabilities = []
+        self.labels = {}
+
+        self.prob_true = Prob_Tracker.Subject(1, self.p0)
+        self.prob_false = Prob_Tracker.Subject(0, self.p0)
+
+        self.current_label = None
+        self.current_score = self.p0
+        self.max_prob_history = [self.p0]
+
+        self.n_seen = 0
+
+    def addClassification(self, cl, user_agent):
+        annotation = int(cl['annotation'])
+        s_score = self.getScore()
+        u_score = user_agent.getScore(annotation)
+
+        self.annotations.append(annotation)
+        self.user_scores.append(u_score)
+        self.n_seen += 1
+
+        u_score_1 = user_agent.getScore(1)
+        u_score_0 = user_agent.getScore(0)
+        # s_score already defined ^^^
+
+        score = self.calculateScore(annotation, u_score_0,
+                                    u_score_1, s_score)
+
+
+
+
+        # Decide which tracker to user
+        if cl['gold_label'] == 1:
+            prob = prob_true
+        elif cl['gold_label'] == 0:
+            prob = prob_false
+
+        # Add classification to tracker
+        prob.addClassification(cl['annotation'])
+        # Calculate and store new user probability
+        prob.calculateScore()
+
+    def getHistory(self):
+        pass
+
+    def calculateScore(annotation, u_score_0, u_score_1, s_score):
+        """
+            s: subject score
+            u1: user probability annotates 1
+            u0: user probability annotates 0
+
+            Calculation when annotation 1
+                      s*u1
+            -------------------------
+            s*u1 (s*u1 + (1-u0)*(1-s))
+
+
+            Calculation when annotation 0
+                      s (1-u1)
+            -------------------------
+            s*(1-u1) (s*(1-u1) + u0*(1-s))
+        """
+        if annotation == 1:
+            a = s_score * u_score_1
+            b = 1 - u_score_0
+            c = 1 - s_score
+
+        elif annotation == 0:
+            a = s_score * (1 - u_score_1)
+            b = u_score_0
+            c = 1 - s_score
+
+        score = a / (a + (b * c))
+        return score
--- swap/swap/agents/tracker.py
+++ swap/swap/agents/tracker.py
@@ -24,3 +24,28 @@ class Prob_Tracker:
         self.n_seen += 1
         if matched:
             self.n_matched += 1
+
+
+class Prob_Tracker_User(Prob_Tracker):
+
+    def __init__(self, label, epsilon):
+        self.label = label
+
+        self.p_history = []
+        self.p_current = epsilon
+
+        self.n_seen = 0
+        self.n_matched = 0
+
+    def calculateScore(self):
+        p = self.n_seen / self.n_matched
+
+        self.p_history.append(p)
+        self.p_current = p
+
+        return p
+
+    def addClassification(self, matched):
+        self.n_seen += 1
+        if matched:
+            self.n_matched += 1
