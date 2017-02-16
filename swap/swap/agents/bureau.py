30b1f2824c21314763acc409fbf97ed46f7bbdd3
Marco Willi <will5448@umn.edu>
2017-02-16 10:26:57 -0600

    Added some unimplemented methods to the bureau class

--- swap/swap/agents/bureau.py
+++ swap/swap/agents/bureau.py
@@ -1,2 +1,38 @@
 ################################################################
 # Keeps track of all user and subject agents
+# - Initial class to test SWAP
+
+
+class Bureau(object):
+    """ Bureau to keep track of all agents of a specific type
+    """
+
+    def __init__(self, agent_type):
+        # type of agents, just a string? (e.g. users, subjects, machines,...)
+        self.agent_type = agent_type
+        
+        # dictionary to store all agents, key is agent-ID
+        self.agents = dict()
+        
+    
+    def addAgent(self, agent):
+        """ Add agent to bureau
+        
+        Parameter:
+        ----------
+            agent: agent object
+        """
+        
+        pass
+    
+    def getAgent(self,agent_id):
+        """ Get agent from bureau
+        
+        Parameter:
+        ----------
+            agent_id: id of agent
+        """
+        
+        pass
+        
+    
