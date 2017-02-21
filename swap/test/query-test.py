98ca73bb8535f94a639ea913532cd7bb33e25c09
Michael Laraia <micheal.laraia@gmail.com>
2017-02-20 19:54:46 -0600

    changed query tests to match class structure

--- swap/test/query-test.py
+++ swap/test/query-test.py
@@ -11,19 +11,19 @@ from collections import OrderedDict
 class Test_Query:
     ################################################################
     # Limit
-    def test_limit():
+    def test_limit(self):
         q = Query()
         q.limit(5)
         assert q._pipeline[-1] == {'$limit': 5}
 
     ################################################################
     # Match
-    def test_match_eq():
+    def test_match_eq(self):
         q = Query()
         q.match('key', 'value')
         assert q._pipeline[-1] == {'$match': {'key': 'value'}}
 
-    def test_match_neq():
+    def test_match_neq(self):
         q = Query()
         q.match('key', 'value', False)
 
@@ -31,19 +31,19 @@ class Test_Query:
 
     ################################################################
     # Project
-    def test_add_new_field():
+    def test_add_new_field(self):
         q = Query()
         q.project([('name', 100)])
 
         assert q._pipeline[-1] == {'$project': {'name': {'$literal': 100}}}
 
-    def test_project_str():
+    def test_project_str(self):
         q = Query()
         q.project('name')
 
         assert q._pipeline[-1] == {'$project': {'name': 1}}
 
-    def test_add_fields_list():
+    def test_add_fields_list(self):
         q = Query()
         fields = ['field1', 'field2', 'field3']
         q.project(fields)
@@ -51,7 +51,7 @@ class Test_Query:
         assert q._pipeline[-1] == {'$project': {'field1': 1,
                                    'field2': 1, 'field3': 1}}
 
-    def test_add_fields_set():
+    def test_add_fields_set(self):
         q = Query()
         fields = {'field1', 'field2', 'field3'}
         q.project(fields)
@@ -59,7 +59,7 @@ class Test_Query:
         assert q._pipeline[-1] == {'$project': {'field1': 1,
                                    'field2': 1, 'field3': 1}}
 
-    def test_add_fields_dict():
+    def test_add_fields_dict(self):
         q = Query()
         fields = {'field1': '$field1', 'field2': '$field2'}
         q.project(fields)
@@ -69,7 +69,7 @@ class Test_Query:
     ################################################################
     # Out
 
-    def test_out():
+    def test_out(self):
         q = Query()
         q.out('collection')
 
@@ -77,13 +77,13 @@ class Test_Query:
 
     ################################################################
     # Group
-    def test_group_str():
+    def test_group_str(self):
         q = Query()
         q.group("field")
 
         assert q._pipeline[-1] == {'$group': {"_id": "$field"}}
 
-    def test_group_list():
+    def test_group_list(self):
         q = Query()
         q.group(['field1', 'field2'])
 
@@ -91,14 +91,14 @@ class Test_Query:
                                    'field1': '$field1',
                                    'field2': '$field2'}}}
 
-    def test_group_list():
+    def test_group_list(self):
         q = Query()
         q.group(['field1', 'field2'])
 
         assert q._pipeline[-1] == {'$group': {"_id":
                                    {'field1': '$field1', 'field2': '$field2'}}}
 
-    def test_group_count():
+    def test_group_count(self):
         q = Query()
         q.group('field',count=True)
 
@@ -110,13 +110,13 @@ class Test_Group:
     # ###############################################################
     # Group Class
 
-    def test_Group_str():
+    def test_Group_str(self):
         g = Group().id('field')
 
         assert g._id == '$field'
         assert g.build() == {'$group': {'_id': '$field'}}
 
-    def test_Group_list():
+    def test_Group_list(self):
         g = Group().id(['field1', 'field2', 'field3'])
 
         _id = {'field1': '$field1', 'field2': '$field2', 'field3': '$field3'}
@@ -124,20 +124,20 @@ class Test_Group:
         assert g._id == _id
         assert g.build() == {'$group': {'_id': _id}}
 
-    def test_Group_count():
+    def test_Group_count(self):
         g = Group().id('field').count()
 
         count = {'$sum': 1}
         assert g._extra['count'] == count
         assert g.build() == {'$group': {'_id': '$field', 'count': {'$sum': 1}}}
 
-    def test_Group_push_one():
+    def test_Group_push_one(self):
         g = Group().id('field').push('name', 'pushed')
 
         assert g._extra['name'] == {'$push': '$pushed'}
         assert g.build()['$group']['name'] == {'$push': '$pushed'}
 
-    def test_Group_push_many():
+    def test_Group_push_many(self):
         fields = ['field1', 'field2', 'field3']
         g = Group().id('id').push('name', fields)
 
@@ -145,7 +145,7 @@ class Test_Group:
         assert g._extra['name'] == {'$push': {'field1': '$field1',
                                     'field2': '$field2', 'field3': '$field3'}}
 
-    def test_group_Group():
+    def test_group_Group(self):
         g = Group().id('id')
         q = Query().group(g)
 
@@ -156,13 +156,13 @@ class Test_Group:
     ################################################################
     # Sort Class
 
-    def test_Sort_one():
+    def test_Sort_one(self):
         s = Sort()
         s.add('field', 1)
 
         assert s._order['field'] == 1
 
-    def test_Sort_two_order():
+    def test_Sort_two_order(self):
         s = Sort()
         s.add('field1', 1).add('field2', -1)
 
@@ -170,14 +170,14 @@ class Test_Group:
         assert order[0] == 'field1'
         assert order[1] == 'field2'
 
-    def test_Sort_two_values():
+    def test_Sort_two_values(self):
         s = Sort()
         s.add('field1', 1).add('field2', -1)
 
         assert s._order['field1'] == 1
         assert s._order['field2'] == -1
 
-    def test_Sort_many_order():
+    def test_Sort_many_order(self):
         s = Sort()
         s.addMany([('field1', 1), ('field2', -1)])
 
@@ -185,7 +185,7 @@ class Test_Group:
         assert order[0] == 'field1'
         assert order[1] == 'field2'
 
-    def test_Sort_many_order():
+    def test_Sort_many_order(self):
         s = Sort()
         s.addMany([('field1', 1), ('field2', -1)])
 
@@ -193,7 +193,7 @@ class Test_Group:
         assert s._order['field1'] == 1
         assert s._order['field2'] == -1
 
-    def test_Sort_build():
+    def test_Sort_build(self):
         fields = [('field1', 1), ('field2', -1)]
         s = Sort()
         s.addMany(fields)
