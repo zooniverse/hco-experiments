0fd398eab09c5479c98db14dfbc122b37c10da8c
Michael Laraia <micheal.laraia@gmail.com>
2017-03-02 15:23:10 -0600

    restructure tools directory

--- swap/tools/test_config.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-################################################################
-# Script to test config functionality
-
-from swap.config import Config
-
-a = Config()
-
-print(a.s)
