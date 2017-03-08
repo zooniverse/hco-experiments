6b7df6050a226f30adc87d2cddb38e3461cd23cc
Michael Laraia <micheal.laraia@gmail.com>
2017-03-08 15:51:45 -0600

    modified tests to match new swap

--- swap/tests/test_swap.py
+++ swap/tests/test_swap.py
@@ -3,7 +3,7 @@ import pytest
 from pprint import pprint
 
 
-def test_export():
+def test_export_nonempty():
     swap = SWAP(p0=2e-4, epsilon=1.0)
 
     cl1 = {
@@ -22,11 +22,18 @@ def test_export():
 
     swap.processOneClassification(cl1)
     swap.processOneClassification(cl2)
-    pprint(swap.export())
-    assert False
+
+    export = swap.export()
+    pprint(export)
+
+    assert 'users' in export
+    assert 'subjects' in export
+
+    assert 'user_1' in export['users']
+    assert 'user_2' in export['users']
+    assert 'subject_1' in export['subjects']
 
 
-@pytest.mark.skip()
 def test_subject_update_perfect_classifier():
 
     swap = SWAP(p0=2e-4, epsilon=1.0)
@@ -34,57 +41,61 @@ def test_subject_update_perfect_classifier():
     cl = {
         'user_name': 'user_1',
         'subject_id': 'subject_1',
-        'annotation': '1',
-        'gold_label': '1'
+        'annotation': 1,
+        'gold_label': 1
     }
 
     swap.processOneClassification(cl)
-    print(swap.getSubjectData())
+    export = swap.exportSubjectData()
+    pprint(export)
 
-    assert swap.getSubjectData()['subject_1']['current_max_prob'] == 1
+    assert export['subject_1']['score'] == 1
 
     cl = {
         'user_name': 'user_1',
         'subject_id': 'subject_2',
-        'annotation': '0',
-        'gold_label': '0'
+        'annotation': 0,
+        'gold_label': 0
     }
 
     swap.processOneClassification(cl)
-    print(swap.getSubjectData())
+    export = swap.exportSubjectData()
+    pprint(export)
 
-    assert swap.getSubjectData()['subject_2']['current_max_prob'] == 0
+    assert export['subject_2']['score'] == 0
 
-@pytest.mark.skip()
+
 def test_subject_update_obtuse_classifier():
 
     swap = SWAP(p0=2e-4, epsilon=0.0)
 
     cl = {
-        'user_name'  : 'user_1',
-        'subject_id' : 'subject_1',
-        'annotation' : '0',
-        'gold_label' : '1'
+        'user_name': 'user_1',
+        'subject_id': 'subject_1',
+        'annotation': 0,
+        'gold_label': 1
     }
 
     swap.processOneClassification(cl)
-    print(swap.getSubjectData())
+    export = swap.exportSubjectData()
+    pprint(export)
 
-    assert swap.getSubjectData()['subject_1']['current_max_prob'] == 1
+    assert export['subject_1']['score'] == 1
 
     cl = {
-        'user_name'  : 'user_1',
-        'subject_id' : 'subject_2',
-        'annotation' : '1',
-        'gold_label' : '0'
+        'user_name': 'user_1',
+        'subject_id': 'subject_2',
+        'annotation': 1,
+        'gold_label': 0
     }
 
     swap.processOneClassification(cl)
-    print(swap.getSubjectData())
+    export = swap.exportSubjectData()
+    pprint(export)
 
-    assert swap.getSubjectData()['subject_2']['current_max_prob'] == 0
+    assert export['subject_2']['score'] == 0
 
-@pytest.mark.skip()
+
 def my_volunteer_update(user, annotation, gold_label):
         user[gold_label]['tot'] += 1
         if gold_label == annotation:
@@ -93,7 +104,7 @@ def my_volunteer_update(user, annotation, gold_label):
             user[gold_label]['correct'] / float(user[gold_label]['tot'])
         return user
 
-@pytest.mark.skip()
+
 def my_subject_update(p0, user, annotation):
 
     if annotation == '1':
@@ -102,58 +113,62 @@ def my_subject_update(p0, user, annotation):
         S = p0*(1-user['1']['p']) / (p0*(1-user['1']['p']) + (user['0']['p'])*(1-p0))
     return S
 
-@pytest.mark.skip()
-def test_subject_update_apply_one_correct_classification():
 
+def predict_subject_score(p0, user0, user1, annotation):
+    if annotation == 1:
+        return p0 * user1 / (p0 * user1 + (1 - user0) * (1 - p0))
+    elif annotation == 0:
+        return p0 * (1 - user1) / (p0 * (1 - user1) + user0 * (1 - p0))
+
+
+def test_subject_update_apply_one_correct_classification():
     p0 = 0.2
     epsilon = 0.5
-    annotation = '1'
-    gold_label = '1'
+    annotation = 1
+    gold_label = 1
+
+    expected_subject_score = predict_subject_score(p0, epsilon, 1, 1)
+    assert expected_subject_score == 1 / 3
 
     swap = SWAP(p0=p0, epsilon=epsilon)
 
     cl = {
-        'user_name'  : 'user_1',
-        'subject_id' : 'subject_1',
-        'annotation' : annotation,
-        'gold_label' : gold_label
-    }
-
-    user = {
-        '1':{'tot':0,'correct':0, 'p':epsilon},
-        '0':{'tot':0,'correct':0, 'p':epsilon}
+        'user_name': 'user_1',
+        'subject_id': 'subject_1',
+        'annotation': annotation,
+        'gold_label': gold_label
     }
 
     swap.processOneClassification(cl)
-    user = my_volunteer_update(user, annotation, gold_label)
-    assert swap.getSubjectData()['subject_1']['current_max_prob'] == my_subject_update(p0,user,annotation)
+    export = swap.exportSubjectData()
+    pprint([export, expected_subject_score])
 
-@pytest.mark.skip()
+    assert export['subject_1']['score'] == expected_subject_score
+
+
 def test_subject_update_apply_one_incorrect_classification():
 
     p0 = 0.2
     epsilon = 0.5
-    annotation = '0'
-    gold_label = '1'
+    annotation = 0
+    gold_label = 1
+
+    expected_subject_score = predict_subject_score(p0, epsilon, 0, 0)
 
     swap = SWAP(p0=p0, epsilon=epsilon)
 
     cl = {
-        'user_name'  : 'user_1',
-        'subject_id' : 'subject_1',
-        'annotation' : annotation,
-        'gold_label' : gold_label
-    }
-
-    user = {
-        '1':{'tot':0,'correct':0, 'p':epsilon},
-        '0':{'tot':0,'correct':0, 'p':epsilon}
+        'user_name': 'user_1',
+        'subject_id': 'subject_1',
+        'annotation': annotation,
+        'gold_label': gold_label
     }
 
     swap.processOneClassification(cl)
-    user = my_volunteer_update(user, annotation, gold_label)
-    assert swap.getSubjectData()['subject_1']['current_max_prob'] == my_subject_update(p0,user,annotation)
+    export = swap.exportSubjectData()
+    pprint([export, expected_subject_score])
 
+    assert export['subject_1']['score'] == expected_subject_score
 
 def main():
 
