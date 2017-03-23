3c1f7f68e5e42d8f8bf2e27814b2388d00c65c86
Michael Laraia <micheal.laraia@gmail.com>
2017-03-23 09:41:20 -0500

    starting to implement bootstrap schema

--- swap/sandbox/bootstrap/main.py
+++ swap/sandbox/bootstrap/main.py
@@ -4,39 +4,92 @@
 # subject labels
 
 from swap.control import Control
-from swap.swap import SWAP
+from swap.mongo.query import Query
+from swap.mongo.db import DB
+from swap.agents.subject import Subject
+
+from swap import ui
+
+from pprint import pprint
 
 
 def main():
-    pass
+    # > db.classifications.aggregate([{$group:{_id:'$subject_id'}},{$sample:{size:5}}])
+    gold_subjects = [3328040, 3313220, 2977121, 2943566, 3317607]
+    gold_subjects += [3624432, 3469678, 3287492, 3627326, 3724438]
+
+    db = DB()
+    golds = db.getExpertGold(gold_subjects)
+    print(golds)
+
+    control = BootstrapControl(.01, .5, golds)
+    ui.run(control)
 
 
 class BootstrapControl(Control):
-    
+
     def __init__(self, p0, epsilon, golds):
-        pass
+        super().__init__(p0, epsilon)
+        self.golds = golds
+
+        bureau = self.swap.subjects
+        for subject, label in golds:
+            agent = Subject(subject, p0, label)
+            bureau.addAgent(agent)
+        self.swap.subjects = bureau
 
     def getClassifications(self):
-        pass
-
-    def process(self):
-        pass
+        golds = [item[0] for item in self.golds]
+        return BootstrapCursor(self._db, golds)
 
 
 class BootstrapCursor:
-    def __init__(self, golds):
+    def __init__(self, db, golds):
         # Create the gold cursor
         # Create the cursor for all remaining classifications
-        pass
+
+        fields = ['user_name', 'subject_id', 'annotation', 'gold_label']
+        query = Query().match('subject_id', golds).project(fields)
+        cursor1 = db.getClassifications(query)
+
+        fields = ['user_name', 'subject_id', 'annotation']
+        query = Query().match('subject_id', golds, eq=False)
+        # query._pipeline.append({'$match': {'classification_id': {'$lt': 1321700}}})
+        query.project(fields)
+        cursor2 = db.getClassifications(query)
+
+        self.cursors = (cursor1, cursor2)
+        self.state = 0
 
     def __iter__(self):
         return self
+
+    def __next__(self):
+        return self.next()
 
     def next(self):
         # First iterate through gold cursor
         # Iterate through other cursor once gold is depleted
         # http://anandology.com/python-practice-book/iterators.html#the-iteration-protocol
-        pass
+        if self.state > 1:
+            raise StopIteration()
+
+        cursor = self.cursors[self.state]
+        if cursor.alive:
+            return cursor.next()
+        else:
+            self.state += 1
+            return self.next()
+
+
+def test():
+    swap = Control(.01, .5)
+    db = swap._db
+    golds = [2149031, 2149962]
+
+    cursor = BootstrapCursor(db, golds)
+    for i in cursor:
+        pprint(i)
 
 
 if __name__ == "__main__":
---
+++ swap/swap/ui.py
@@ -0,0 +1,177 @@
+################################################################
+
+from swap.control import Control
+import pickle
+from pprint import pprint
+import matplotlib.pyplot as plt
+import argparse
+
+
+def run(control):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-p', action='store_true',
+        help='Run the SWAP algorithm again, and pickle and save')
+    parser.add_argument(
+        '-s', nargs=1,
+        help='Generate subject track plot and output to filename S')
+    parser.add_argument(
+        '-u', nargs=1,
+        help='Generate user track plots and output to filename U')
+    parser.add_argument(
+        '--output', nargs=1,
+        help='Not instantiated yet')
+    parser.add_argument(
+        'pickle', help='The filename of the pickled SWAP export')
+
+    args = parser.parse_args()
+
+    if args.p:
+        data = run_swap(control, args.pickle)
+    else:
+        data = load_pickle(args.pickle)
+
+    if args.s:
+        plot_subjects(data, args.s[0])
+
+    if args.u:
+        plot_users(data, args.u[0])
+
+    if args.output:
+        write_output(data, args.output[0])
+
+
+def load_pickle(fname):
+    with open(fname, 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+
+def run_swap(control, fname):
+    # control = Control(.01, .5)
+    control.process()
+
+    data = control.getSWAP().export()
+    # with open('log', 'w') as file:
+    #     yaml.dump(data, file)
+
+    with open(fname, 'wb') as file:
+        pickle.dump(data, file)
+
+    return data
+
+
+def find_errors():
+    with open('pickle.pkl', 'rb') as file:
+        data = pickle.load(file)
+
+    interest = []
+    for key, value in data['subjects'].items():
+        if 1.0 in value['history'] or 0 in value['history']:
+            interest.append(value)
+
+    print(len(interest))
+    plot_subjects(data,
+                  "Subject Tracks",
+                  "subject_tracks.png")
+
+    with open('log', 'w') as file:
+        pprint(interest, file)
+
+
+def combine_user_scores(export, fname):
+    data = []
+    for item in export['users'].values():
+        h0 = item['score_0_history']
+        h1 = item['score_1_history']
+
+        h = []
+        for i, v0 in enumerate(h0):
+            if len(h1) <= i:
+                v1 = h1[-1]
+            else:
+                v1 = h1[i]
+            h.append((v0 + v1) / 2)
+        data.append((1, h))
+
+    name = fname.split('.')
+    name[0] += '-combined'
+    name = '.'.join(name)
+
+    plot_tracks(data, 'User Combined Tracks', name, scale='linear')
+
+
+def plot_users(export, fname):
+    def getData(data, n):
+        data = []
+        for item in export['users'].values():
+            h = item['score_%d_history' % n]
+            if len(h) > 7:
+                if len(h) > 20:
+                    data.append((n, h[7:20]))
+                else:
+                    data.append((n, h[7:]))
+        return data
+
+    def plotData(export, n, fname):
+        name = fname.split('.')
+        name[0] += '-%d' % n
+        name = '.'.join(name)
+        plot_tracks(
+            getData(export, n),
+            'User %d Tracks' % n,
+            name, scale='linear')
+
+    plotData(export, 1, fname)
+    plotData(export, 0, fname)
+    combine_user_scores(export, fname)
+
+
+def plot_subjects(export, fname):
+    print(fname)
+    data = [(d['gold_label'], d['history']) for d in export['subjects'].values()]
+    plot_tracks(data, 'Subject Tracks', fname)
+
+
+def plot_tracks(data, title, fname, dpi=600, scale='log'):
+    """ Plot subject tracks """
+    cmap = ["#669D31", "#F00200"]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    count = 0
+    for gold, history in data:
+        if count == 1000:
+            break
+
+        ax.plot(
+            [0.01] + history,
+            range(len(history) + 1),
+            "-",
+            color=cmap[gold],
+            lw=1,
+            alpha=0.2)
+
+        count += 1
+
+    plt.xlim(-0.01, 1.01)
+    ax.set_yscale(scale)
+    plt.gca().invert_yaxis()
+
+    plt.xlabel("P(real)")
+    plt.ylabel("number of classificaions")
+    plt.title(title)
+
+    plt.savefig(fname, dpi=dpi)
+    # plt.show()
+
+
+def write_output(data, fname):
+    with open(fname, 'w') as file:
+        pprint(data, file)
+
+
+if __name__ == "__main__":
+    # run()
+    pass
