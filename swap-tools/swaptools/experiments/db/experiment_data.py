c8a1b64f89d3b67e2b1a3a62a26a388a749f9111
Michael Laraia <micheal.laraia@gmail.com>
2017-06-05 14:18:29 -0500

    Cursor to get trials of experiment

--- swap-tools/swaptools/experiments/db/experiment_data.py
+++ swap-tools/swaptools/experiments/db/experiment_data.py
@@ -2,6 +2,7 @@
 # Methods for experiment collection
 
 from swaptools.experiments.db import DB
+from swap.db import Cursor
 
 collection = DB().data
 aggregate = collection.aggregate
@@ -13,3 +14,18 @@ def upload_trials(trials, experiment_name):
         data += trial.db_export(experiment_name)
 
     collection.insert_many(data)
+
+
+def get_trials(experiment_name, group=None):
+    assert group
+    query = [
+        {'$match': {'experiment': experiment_name}},
+        {'$group': {'_id': {'controversial': '$trial.controversial',
+                            'consensus': '$trial.consensus'},
+                    'points': {'$push': {'subject': '$subject',
+                                         'gold': '$gold',
+                                         'p': '$p', 'used_gold':
+                                         '$used_gold'}}}}
+    ]
+
+    return Cursor(query, collection, allowDiskUse=True)
