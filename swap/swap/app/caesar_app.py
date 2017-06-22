0127c0ae9bb67d409fcf7c71951da5f83f856205
Michael Laraia <micheal.laraia@gmail.com>
2017-06-22 16:16:18 +0100

    Prepare basic caesar app

---
+++ swap/swap/app/caesar_app.py
@@ -0,0 +1,70 @@
+
+# Collection of infrastructures to run SWAP in an online state
+# with Caesar
+
+from swap.app.control import OnlineControl
+
+import logging
+from flask import Flask, request
+
+logger = logging.getLogger(__name__)
+
+
+# Actions we might take to notify caesar
+
+# retire subject
+
+# post score changes
+
+# change subject workflow
+
+
+# API so Caesar can notify us
+
+# receive classification
+"""
+    Data required in extraction:
+    unique user identifier (whether that's user_name or user_id)
+    subject identifier (subject_id, object_id)
+    annotation
+    gold label
+        Should we rely on Caesar to maintain gold labels for us?
+        Do we need to push new gold labels to Caesor if so?
+        Might be better to maintain them in our local db
+
+    Should store the incoming classificadtion in our db
+        Do we need validation checks to ensure we don't have
+        duplicate classificationsin our db
+    Should then recalculate scores
+
+    Do we then need to respond with the updated for that subject?
+        Would it be better to just send our own asynchronous
+        retirement message when we see fit?
+"""
+
+
+"""
+TODO:
+    Write online controller
+        Listen as reducer and process incoming classifications
+        Pre-process classifications already in database
+    Write request/response schema
+    Write new database methods
+        To store incoming classifications in cl database
+"""
+
+app = Flask(__name__)
+
+
+@app.route('/classify')
+def classify():
+    classification = OnlineControl.parse_classification(request.args)
+    OnlineControl().classify(classification)
+
+
+def run():
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+if __name__ == '__main__':
+    run()
---
+++ swap/swap/app/control.py
@@ -0,0 +1,60 @@
+
+import swap.control
+from swap.utils.classification import Classification
+from swap.utils import Singleton
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def parse_classification(data):
+    annotation = parse_annotation(data['annotations'])
+
+    params = {
+        'subject': data['subject_id'],
+        'user': data['user_id'],
+        'annotation': annotation
+    }
+    classification = Classification(**params)
+    return classification
+
+
+def parse_annotation(annotations):
+    # TODO parsing annotations for multiple tasks
+    logger.debug('parsing annotations: %s', str(annotations))
+    value = list(annotations.values())[0][0]['value']
+    print(value)
+    return value
+
+
+class _OnlineControl(swap.control.Control):
+    """
+    Controller for SWAP in online mode
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.run()
+        logger.debug('Initialized online controller')
+
+    def subjects_changed(self):
+        # return subjects whose scores have changed
+        pass
+
+    @staticmethod
+    def parse_classification(args):
+        return parse_classification(args)
+
+    def classify(self, classification):
+        # Add classification from caesar
+        logger.debug('Adding classification from network: %s',
+                     str(classification))
+        self.swap.classify(classification)
+
+        subject = self.swap.subjects.get(classification.subject)
+        return subject.score
+
+
+class OnlineControl(_OnlineControl, metaclass=Singleton):
+    pass
