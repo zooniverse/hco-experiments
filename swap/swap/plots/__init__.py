0f4b421b97e9e83903d9b9423db93630784c0faf
Michael Laraia <micheal.laraia@gmail.com>
2017-06-30 11:26:39 +0100

    Prevent plots import if matplotlib is unavailable

--- swap/swap/plots/__init__.py
+++ swap/swap/plots/__init__.py
@@ -1,23 +1,21 @@
 ################################################################
 
-from swap.plots import traces, distributions, performance
+try:
+    from swap.plots import traces, distributions, performance
 
-from swap.plots.distributions import plot_pdf
-from swap.plots.distributions import plot_class_histogram
+    from swap.plots.distributions import plot_pdf
+    from swap.plots.distributions import plot_class_histogram
 
-from swap.plots.performance import plot_user_cm
-from swap.plots.performance import plot_confusion_matrix
-# from swap.plots.performance import plot_histogram
-from swap.plots.performance import plot_roc
-
-# 'using' imports to silence pep warnings
-assert traces
-assert distributions
-assert performance
-
-assert plot_pdf
-
-assert plot_user_cm
-assert plot_confusion_matrix
-assert plot_class_histogram
-assert plot_roc
+    from swap.plots.performance import plot_user_cm
+    from swap.plots.performance import plot_confusion_matrix
+    # from swap.plots.performance import plot_histogram
+    from swap.plots.performance import plot_roc
+except ImportError:
+    traces = None
+    distributions = None
+    performance = None
+    plot_pdf = None
+    plot_class_histogram = None
+    plot_user_cm = None
+    plot_confusion_matrix = None
+    plot_roc = None
