4d96123db7465704eadce266c314a22ec24b013e
Michael Laraia <micheal.laraia@gmail.com>
2017-07-12 11:19:36 +0100

    Update tests

--- swap/tests/caesar/app_test.py
+++ swap/tests/caesar/app_test.py
@@ -2,6 +2,7 @@
 import swap.caesar.control as control
 import swap.caesar.app as app
 from swap.caesar.utils.address import Address
+from swap.caesar.utils.requests import Requests
 import swap.agents.subject
 from swap.db import DB
 from swap.db.db import Collection
@@ -28,9 +29,11 @@ class TestCaesarApp:
     @patch.object(GoldGetter, 'golds', {})
     @patch('swap.config.back_update', False)
     @patch('swap.config.database.name', 'swapDBtest')
+    @patch('swap.config.parser.annotation.task', 'T1')
     @patch('swap.config.parser.annotation.true', [1])
     @patch('swap.config.parser.annotation.false', [0])
     def test_parse_annotation(self, run):
+        print(self.mock_classification)
         oc = control.OnlineControl()
         cl = oc.parse_raw(self.mock_classification)
         cl = oc.gen_cl(cl)
@@ -44,8 +47,10 @@ class TestCaesarApp:
     @patch.object(control.OnlineControl, 'run')
     @patch.object(GoldGetter, 'golds', {})
     @patch.object(Classifications, 'insert', MagicMock())
+    @patch.object(Classifications, 'exists', MagicMock(return_value=False))
     @patch('swap.config.back_update', False)
     @patch('swap.config.database.name', 'swapDBtest')
+    @patch('swap.config.parser.annotation.task', 'T1')
     @patch('swap.config.parser.annotation.true', [1])
     @patch('swap.config.parser.annotation.false', [0])
     def test_classify(self, run):
@@ -57,15 +62,36 @@ class TestCaesarApp:
         assert isinstance(ret, swap.agents.subject.Subject)
         assert ret.score == 0.12
 
+    @patch.object(control.OnlineControl, 'run')
+    @patch.object(GoldGetter, 'golds', {})
+    @patch.object(Classifications, 'insert', MagicMock())
+    @patch.object(Classifications, 'exists', MagicMock(return_value=True))
+    @patch('swap.config.back_update', False)
+    @patch('swap.config.database.name', 'swapDBtest')
+    @patch('swap.config.parser.annotation.task', 'T1')
+    @patch('swap.config.parser.annotation.true', [1])
+    @patch('swap.config.parser.annotation.false', [0])
+    def test_classify_rejects_reclassify(self, run):
+        DB._reset()
+        oc = control.OnlineControl()
+        oc.init_swap()
+        ret = oc.classify(self.mock_classification)
+
+        assert ret is None
+
     @patch('swap.config.online_swap.workflow', '1234')
+    @patch('swap.config.online_swap.caesar.host', 'example.com')
+    @patch('swap.config.online_swap.caesar.port', '2000')
     def test_reducer_address(self):
-        address = 'https://caesar-staging.zooniverse.org:443/' \
+        address = 'https://example.com:2000/' \
                   'workflows/1234/reducers/swap/reductions'
         assert Address.reducer() == address
 
     @patch('swap.config.online_swap.workflow', '1234')
+    @patch('swap.config.online_swap.caesar.host', 'example.com')
+    @patch('swap.config.online_swap.caesar.port', '2000')
     def test_root_address(self):
-        address = 'https://caesar-staging.zooniverse.org:443/' \
+        address = 'https://example.com:2000/' \
                   'workflows/1234'
         assert Address.root() == address
 
@@ -84,7 +110,7 @@ class TestCaesarApp:
             'rules_config': []
         }}
 
-        assert Address.config_caesar() == data
+        assert Requests.config_caesar() == data
 
     def test_recent_cl_full(self):
         api = app.API(None)
